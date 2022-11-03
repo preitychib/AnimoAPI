c875f1e4533ab83659e072815def31fe9bb09135
preitychib <preeti.chib2015@gmail.com>
2022-11-03 13:32:01 +0530

    edited staticfile storage

--- AnimoAPI/settings.py
+++ AnimoAPI/settings.py
@@ -137,8 +137,8 @@ STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
-STATICFILES_STORAGE= 'whitenoise.django.GzipManifestStaticFilesStorage'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE= 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
